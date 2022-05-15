bb91b1e7fc23b442284ec821987cd059f4fd5f2d
Amber Alter <aarnold@Ambers-MacBook-Air.local>
2022-05-15 00:46:03 -0700

    display restaurant name from google sheets

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-var restaurants = []
+//var restaurants;
 const apiKey = "AIzaSyC1zmiuAFdXP3wytulS0AOxRRjPl6smf_o";
 const spreadsheetId = "1qXVoKMWnMvdVq2fhikqdwgSbVo86VVfFoOfDuwg5bPs";
 const range = "Sheet1!A1:C";
@@ -6,36 +6,27 @@ const url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/valu
 async function fetchData(url, callback) {
    const data = await fetch(url)
     .then(response => response.json())
-    .then(data => {return data;} )
-
-    callback(data);
+    .then(data => {callback(data)} )
 }
 
-function randomRestaurantArray (data) {
-    const values = data.values;
-    const labels = values[0];
-    const details = values.slice(1);
-    restaurants.push(labels, details);
-
+function randomRestaurantArray (data) { 
+   const values = data.values;
+   const details = values.slice(1);
+   getRandomRestaurant(details);
 }
-fetchData(url, randomRestaurantArray);
-
-console.log("restaurants:", restaurants);
 
 function randomRestaurant(restaurant){
     const restaurantName = document.querySelector("#random-restaurant_name");
-    restaurantName.textContent = restaurant.name;
+    restaurantName.textContent = restaurant[0];
 }
 
-function getRandomRestaurant(array){
-    const randomIndex = Math.floor(Math.random() * array.length);
-    const restaurant = array[randomIndex];
+function getRandomRestaurant(obj){
+    const randomIndex = Math.floor(Math.random() * obj.length);
+    const restaurant = obj[randomIndex];
     randomRestaurant(restaurant);
 }
 
 const generatorBtn = document.querySelector("#random-restaurant-btn");
 generatorBtn.addEventListener("click", function (e){
-    getRandomRestaurant(restaurants);
-})
-
-//console.log(printRandomRestaurant(randomRestaurant));
+    fetchData(url, randomRestaurantArray);
+});
