8a9ec80d3153bba2ea3eda5d663ba968702f5baa
Amber Alter <amalter@users.noreply.github.com>
2022-05-18 14:21:20 -0700

    create function to clear selectAll box, set all inputs to checked by default

--- js/filters.js
+++ js/filters.js
@@ -4,6 +4,12 @@
  *
  */
 
+/**
+ *
+ * Declare & assign form and input variables
+ *
+ * */
+
 const form = document.querySelector("#restaurant-filter_form");
 const checkboxes = document.querySelectorAll(
   ".filters-fieldset input[type='checkbox']:not(#all)"
@@ -12,26 +18,35 @@ const selectAll = document.querySelectorAll(
   ".filters-fieldset input[id='all']"
 );
 
-console.log("selectAll", selectAll[0]);
-
+/**-----------------------------------------------
+ *
+ * Event listner on form submit
+ *
+ */
 form.addEventListener("submit", function (e) {
   handleForm(e, checkboxes);
 });
+
+/**
+ * Prevents default reload of page on submit click
+ * builds an array of selected filters
+ *
+ * @param {*} e
+ * @param {*} checkboxes
+ */
 function handleForm(e, checkboxes) {
   e.preventDefault();
   let filtersArray = selectedFilters(checkboxes);
-  console.log("checkboxes", checkboxes);
   console.log("filtersArray", filtersArray);
 }
 
 /**
  *
- * Builds out an object containing selected neighborhood and cuisine filters
+ * selectedFilters function returns object containing selected neighborhood and cuisine filters
  * @param {
  * } inputs
  * @returns
  */
-
 function selectedFilters(checkboxes) {
   var neighborhoods = [];
   var cuisines = [];
@@ -47,12 +62,24 @@ function selectedFilters(checkboxes) {
 
   return { neighborhoods, cuisines };
 }
+
+/**-----------------------------------------------
+ *
+ * Event listner on "Select All" checkbox changes
+ *
+ */
 selectAll.forEach((all) => {
   all.addEventListener("change", function (e) {
     selectAllToggle(all, checkboxes);
   });
 });
 
+/**
+ * selectAllToggle function called on "select all" checkbox change
+ * if "Select All" is checked/unchecked, all other checkboxes will be checked/unchecked
+ * @param {*} all
+ * @param {*} checkboxes
+ */
 function selectAllToggle(all, checkboxes) {
   let classList = all.classList;
   checkboxes.forEach((checkbox) => {
@@ -62,4 +89,28 @@ function selectAllToggle(all, checkboxes) {
   });
 }
 
-function clearSelectAll() {}
+/**-----------------------------------------------
+ *
+ * Event listner on checkbox changes (excluding "Select All")
+ *
+ */
+checkboxes.forEach((checkbox) => {
+  checkbox.addEventListener("change", function (e) {
+    clearSelectAll(checkbox, selectAll);
+  });
+});
+
+/**
+ * clearSelectAll function called on checkbox change
+ * clears check from the "Select All" checkbox if other item is checked in list
+ * @param {*} checkbox
+ * @param {*} selectAll
+ */
+function clearSelectAll(checkbox, selectAll) {
+  let classList = checkbox.classList;
+  selectAll.forEach((allCheckbox) => {
+    if (!checkbox.checked && allCheckbox.classList.contains(classList)) {
+      allCheckbox.checked = checkbox.checked;
+    }
+  });
+}
